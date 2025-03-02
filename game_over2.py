d7ea7f4fccf649d70ac026fddde6a75b78ea7abc
codinganddecoding13 <kdwbusiness96@gmail.com>
2025-03-02 18:02:48 -0500

    Create game_over2.py

---
+++ game_over2.py
@@ -0,0 +1,33 @@
+# Game Over - Version 2
+# Demonstrates the use of quotes in strings
+
+print("Program 'Game Over' 2.0")
+
+print("Same", "message", "as before")
+
+print("Just",
+      "a bit",
+      "bigger")
+
+print("Here", end = " ")
+print("it is...")
+
+print(
+        """
+          _____          _____       _____  _____    ______
+        /   ___|        /     |     /     |/     |  |   ___|
+        |  |           /  /|  |    /  /|     /|  |  |  |___
+        |  |   __     /  ___  |   /  / |____/ |  |  |   ___|
+        |  |__|  |   /  /  |  |  /  /         |  |  |  |___
+        \________/  /__/   |__| /__/          |__|  |______|
+         _______     __       __    _________     ________
+        /  ___  \   |  |    /  /   |   _____|    |  ___   \ 
+        | |   | |   |  |   /  /    |  |____      |  |__|  |
+        | |   | |   |  |  /  /     |   ____|     |   __  /
+        | |___| |   |  | /  /      |  |_____     |  |  \  \ 
+        \_______/   |______/       |________|    |__|   \__\ 
+    
+        """
+    )
+
+input("\n\nPress the enter key to exit.")
