4e5fa3eb39e936e795591bc573a19ddec152e158
codinganddecoding13 <kdwbusiness96@gmail.com>
2025-03-02 18:05:09 -0500

    Create fancy_credits.py

---
+++ fancy_credits.py
@@ -0,0 +1,15 @@
+# Fancy Credits
+# Demonstrates escape sequences
+
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\")
+print("\t\t\t\tby")
+print("\t\t\tMichael Dawson")
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\")
+
+print("\nSpecial thanks goes out to:")
+print("My hair stylist, Henry \'The Great.\' who never says \"cant\'t.\"")
+
+# sound the system bell
+print("\a")
+
+input("\n\nPress the enter key to exit.")
